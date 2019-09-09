5a388da2d6c1dd0c9c179d8e1d6c8c9c26dbf6e3
Bekti Cahyo <Bektic.22@gmail.com>
2019-09-09 08:55:20 +0700

    add assets

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/rumahulya/Development/tools/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/rumahulya/Documents/Sample/FlutterApp/cinema_flt"
-export "FLUTTER_TARGET=/Users/rumahulya/Documents/Sample/FlutterApp/cinema_flt/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/rumahulya/Documents/Sample/FlutterApp/Flutter-Cinema"
+export "FLUTTER_TARGET=/Users/rumahulya/Documents/Sample/FlutterApp/Flutter-Cinema/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/rumahulya/Development/tools/flutter/bin/cache/artifacts/engine/ios"
