a4b6e24896ed69074914d8506bc70bd4d4b4fbda
Bekti Cahyo <Bektic.22@gmail.com>
2019-09-09 10:51:40 +0700

    slider populer movie

--- lib/screens/home/widgets/populer_movie_slider.dart
+++ lib/screens/home/widgets/populer_movie_slider.dart
@@ -1,4 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cinema_flt/components/widgets/text_dot_center.dart';
+import 'package:cinema_flt/utils/AppStyle.dart';
 import 'package:flutter/material.dart';
 
 import 'package:snaplist/snaplist_view.dart';
@@ -27,40 +29,91 @@ class _PopulerMovieSliderState extends State<PopulerMovieSlider> {
           builder: (BuildContext context) {
             return Stack(
               children: <Widget>[
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  margin: EdgeInsets.symmetric(horizontal: 5.0),
-                  decoration: BoxDecoration(
-                      color: Colors.transparent,
-                      borderRadius: BorderRadius.circular(10.0)),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0),
-                    child: Image.asset(
-                      'assets/images/sample_6.jpg',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 5.0),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      gradient: LinearGradient(
-                        begin: FractionalOffset.center,
-                        end: FractionalOffset.bottomCenter,
-                        colors: [
-                          Colors.transparent,
-                          Colors.black.withOpacity(0.5),
-                          Colors.black,
-                        ],
-                      ),
-                      borderRadius: BorderRadius.circular(10.0)),
-                ),
+                _imageView(screenWidth),
+                _contentView(contentHeight),
               ],
             );
           },
         );
       }).toList(),
+    );
+  }
+
+  Widget _imageView(double screenWidth) {
+    return Container(
+      width: screenWidth,
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: Image.asset(
+          'assets/images/sample_6.jpg',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _contentView(double contentHeight) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Container(
+          height: contentHeight / 3,
+          margin: EdgeInsets.symmetric(horizontal: 10),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.black.withOpacity(0.5),
+                Colors.black,
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+          ),
+          child: _titleContent(),
+        ),
+      ],
+    );
+  }
+
+  Widget _titleContent() {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Avanger Endmen',
+            style: TextStyle(
+              color: AppStyle.getColor(ThemeColor.secondary),
+              fontSize: 14,
+            ),
+            textAlign: TextAlign.left,
+          ),
+          SizedBox(
+            height: 3,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              AppStyle.textSubtitle('2h 3m'),
+              TextDotCenter(),
+              AppStyle.textSubtitle('Rate 5'),
+              TextDotCenter(),
+              AppStyle.textSubtitle('8.5'),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
