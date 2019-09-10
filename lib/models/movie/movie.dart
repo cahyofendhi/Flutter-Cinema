79a96355273740d167a2e8d7d50cf54b1c2b7587
Bekti Cahyo <Bektic.22@gmail.com>
2019-09-10 09:23:26 +0700

    add movie model

---
+++ lib/models/movie/movie.dart
@@ -0,0 +1,71 @@
+
+
+class Movie {
+  double popularity;
+  int voteCount;
+  bool video;
+  String posterPath;
+  int id;
+  bool adult;
+  String backdropPath;
+  String originalLanguage;
+  String originalTitle;
+  List<int> genreIds;
+  String title;
+  double voteAverage;
+  String overview;
+  String releaseDate;
+
+  Movie(
+      {this.popularity,
+      this.voteCount,
+      this.video,
+      this.posterPath,
+      this.id,
+      this.adult,
+      this.backdropPath,
+      this.originalLanguage,
+      this.originalTitle,
+      this.genreIds,
+      this.title,
+      this.voteAverage,
+      this.overview,
+      this.releaseDate});
+
+  Movie.fromJson(Map<String, dynamic> json) {
+    popularity = json['popularity'];
+    voteCount = json['vote_count'];
+    video = json['video'];
+    posterPath = json['poster_path'];
+    id = json['id'];
+    adult = json['adult'];
+    backdropPath = json['backdrop_path'];
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    genreIds = json['genre_ids'].cast<int>();
+    title = json['title'];
+    voteAverage = json['vote_average'];
+    overview = json['overview'];
+    releaseDate = json['release_date'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['popularity'] = this.popularity;
+    data['vote_count'] = this.voteCount;
+    data['video'] = this.video;
+    data['poster_path'] = this.posterPath;
+    data['id'] = this.id;
+    data['adult'] = this.adult;
+    data['backdrop_path'] = this.backdropPath;
+    data['original_language'] = this.originalLanguage;
+    data['original_title'] = this.originalTitle;
+    data['genre_ids'] = this.genreIds;
+    data['title'] = this.title;
+    data['vote_average'] = this.voteAverage;
+    data['overview'] = this.overview;
+    data['release_date'] = this.releaseDate;
+    return data;
+  }
+}
+
