f6788750521bd1ab8f64a57e69ec562e806559e2
Bekti Cahyo <Bektic.22@gmail.com>
2019-09-11 00:20:33 +0700

    add loading

---
+++ lib/models/service_model.dart
@@ -0,0 +1,13 @@
+
+
+class ServiceModel<T> {
+  T model;
+  String message = '';
+  String errorMessage = '';
+  bool isSuccess;
+
+  ServiceModel(
+      {this.model, this.errorMessage = '', this.message = ''}) {
+        this.isSuccess = errorMessage.isEmpty && model != null;
+      }
+}
