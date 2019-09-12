9182ee865b654dd48dbef22d81e5e05830c234dc
Bekti Cahyo <Bektic.22@gmail.com>
2019-09-12 09:45:27 +0700

    add db

--- lib/repository/movie_repository.dart
+++ lib/repository/movie_repository.dart
@@ -1,19 +1,29 @@
 import 'package:chopper/chopper.dart';
 import 'package:cinema_flt/data/env.dart';
+import 'package:cinema_flt/db/movie_db.dart';
+import 'package:cinema_flt/db/movie_moor.dart';
+import 'package:cinema_flt/models/movie/movie.dart';
 import 'package:cinema_flt/models/movie/movies_result.dart';
 import 'package:cinema_flt/models/service_model.dart';
 import 'package:cinema_flt/services/service.dart';
 import 'package:flutter/material.dart';
+import 'package:moor_flutter/moor_flutter.dart';
 
 enum MovieCategory { Upcoming, TopRate, Populer }
 
 class MovieRepository {
   final Service _service;
+  final MovieMoor _movieMoor;
 
-  MovieRepository(@required service) : _service = service;
+  MovieRepository(@required service, @required movieMoor)
+      : _service = service,
+        _movieMoor = movieMoor;
 
   Future<ServiceModel> getUpcomingMovie(int page) async {
-    return await getMovie(MovieCategory.Upcoming, page);
+    ServiceModel result = await getMovie(MovieCategory.Upcoming, page);
+    MoviesResult movies = result.model;
+    await insertMovie(datas: movies.results, isUpcoming: true);
+    return result;
   }
 
   Future<ServiceModel> getPopulerMovie(int page) async {
@@ -26,7 +36,8 @@ class MovieRepository {
 
   Future<ServiceModel> getMovie(MovieCategory category, int page) async {
     try {
-      Response response = await _service.getMovieList(_getCategoryMovie(category), API_KEY, 1);
+      Response response =
+          await _service.getMovieList(_getCategoryMovie(category), API_KEY, 1);
       if (response.isSuccessful) {
         return ServiceModel(model: MoviesResult.fromJson(response.body));
       } else {
@@ -36,6 +47,21 @@ class MovieRepository {
       print('Caught ${e.toString()}');
       return ServiceModel(errorMessage: e.toString());
     }
+  }
+
+  Future<void> insertMovie(
+      {List<Movie> datas,
+      bool isPopuler = false,
+      bool isUpcoming = false,
+      bool isTopRate = false}) async {
+    // await _movieMoor.insertMovie(
+    //     datas: datas,
+    //     isPopuler: isPopuler,
+    //     isTopRate: isTopRate,
+    //     isUpcoming: isUpcoming);
+    await _movieMoor.getMovieList(isToprate: true).then((list) {
+      print('Jumlah ${list.length}');
+    });
   }
 
   String _getCategoryMovie(MovieCategory category) {
