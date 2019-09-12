f999eac96be2c07f2eea036fda6852ba6fba64ac
Bekti Cahyo <Bektic.22@gmail.com>
2019-09-12 15:09:56 +0700

    no message

--- lib/db/movie_moor.dart
+++ lib/db/movie_moor.dart
@@ -11,28 +11,17 @@ part 'movie_moor.g.dart';
 class MovieMoor extends DatabaseAccessor<MovieDb> with _$MovieMoorMixin {
   MovieMoor(MovieDb db) : super(db);
 
-  Future insertOrReplaceMovie(MoviesCompanion data) =>
-      into(movies).insert(data, orReplace: true);
+  Future insertOrReplaceMovie(List<MoviesCompanion> data) =>
+      into(movies).insertAll(data);
 
   Future<List<MovieEntry>> getMovieList(
-      {bool isPopuler = false,
-      bool isUpcoming = false,
+      {bool isUpcoming = false,
+      bool isPopuler = false,
       bool isToprate = false}) {
     return (select(movies)
-          ..where(
-            (t) {
-              if (isPopuler) {
-                print("getPopuler");
-                return t.popular.equals(isPopuler);
-              } else if (isUpcoming) {
-                print("getUpcoming");
-                return t.upcoming.equals(isUpcoming);
-              } else {
-                print("getTopRate");
-                return t.topRate.equals(isToprate);
-              }
-            },
-          ))
+          ..where((t) => t.upcoming.equals(isUpcoming))
+          ..where((t) => t.popular.equals(isPopuler))
+          ..where((t) => t.topRate.equals(isToprate)))
         .get();
   }
 
@@ -41,30 +30,32 @@ class MovieMoor extends DatabaseAccessor<MovieDb> with _$MovieMoorMixin {
       bool isPopuler = false,
       bool isUpcoming = false,
       bool isTopRate = false}) async {
-    datas.forEach((dt) {
-      MoviesCompanion companion = MoviesCompanion(
-          video: Value(dt.video),
-          posterPath: Value(dt.posterPath),
-          idMovie: Value(dt.id),
-          voteCount: Value(dt.voteCount),
-          popularity: Value(dt.popularity),
-          adult: Value(dt.adult),
-          backdropPath: Value(dt.backdropPath),
-          originalLanguage: Value(dt.originalLanguage),
-          originalTitle: Value(dt.originalTitle),
-          title: Value(dt.title),
-          voteAverage: Value(dt.voteAverage),
-          genreIds: Value(dt.genreIds.toString()),
-          overview: Value(dt.overview),
-          releaseDate: Value(dt.releaseDate),
-          upcoming: Value(isUpcoming),
-          popular: Value(isPopuler),
-          topRate: Value(isTopRate));
-      insertOrReplaceMovie(companion).then((s) {
-        print('Save success : $s');
-      }).catchError((err) {
-        print('Error Save : ${err.toString()}');
+    List<MoviesCompanion> dataMovie = [];
+    if (datas.isNotEmpty) {
+      datas.forEach((dt) {
+        MoviesCompanion companion = MoviesCompanion(
+            video: Value(dt.video),
+            posterPath: Value(dt.posterPath),
+            idMovie: Value(dt.id),
+            voteCount: Value(dt.voteCount),
+            popularity: Value(dt.popularity),
+            adult: Value(dt.adult),
+            backdropPath: Value(dt.backdropPath),
+            originalLanguage: Value(dt.originalLanguage),
+            originalTitle: Value(dt.originalTitle),
+            title: Value(dt.title),
+            voteAverage: Value(dt.voteAverage),
+            genreIds: Value(dt.genreIds.toString()),
+            overview: Value(dt.overview),
+            releaseDate: Value(dt.releaseDate),
+            upcoming: Value(isUpcoming),
+            popular: Value(isPopuler),
+            topRate: Value(isTopRate));
+        dataMovie.add(companion);
       });
-    });
+      insertOrReplaceMovie(dataMovie).catchError((err) {
+        print("Error Save : ${err.toString()}");
+      });
+    }
   }
 }
